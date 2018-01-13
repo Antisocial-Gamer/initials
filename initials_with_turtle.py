c32caf9fa97d729e5d76513e8b85fb57f72eda77
Antisocial-Gamer <34355423+Antisocial-Gamer@users.noreply.github.com>
2018-01-13 13:21:28 -0500

    Update initials_with_turtle.py
    project is optimised for size 2

--- initials_with_turtle.py
+++ initials_with_turtle.py
@@ -3,24 +3,30 @@ wn = turtle.Screen()
 tur = turtle.Turtle()
 tur.shape("turtle")
 tur.showturtle()
-
+Fast = False
+stop = False
 Bold = False
 tur.left(90)
+tur.penup()
+tur.back(90)
+tur.pendown()
 Color = "blue"
 
 
-def drawj(sizej, Bold, Color):
+def drawj(sizej, Bold, Color, Fast, turtle):
     for i in range(1):
+        if Fast == True:
+            tur.speed(0.50)
         if Bold == True:
             tur.pensize(3)
             sizej = sizej + 1
         if Bold == False:
             tur.pensize(1)
-        if Color == "red":
-            tur.color("red")
+        if Color == "blue":
+            tur.color("blue")
         tur.right(90)
         tur.penup()
-        tur.backward(35 * sizej)
+        tur.backward(60 * sizej)
         tur.forward(1)
         tur.pendown()
         tur.right(90)
@@ -36,23 +42,41 @@ def drawj(sizej, Bold, Color):
         tur.left(90)
         tur.forward(60 * sizej)
         tur.left(90)
-        tur.forward(15 * sizej)
+        tur.forward(10 * sizej)
         tur.left(90)
         tur.pendown()
 
 
 def drawr(sizer):
-    tur.forward(100 * sizer)
+    tur.forward(70 * sizer)
     for i in range(18):
-        tur.right(15 * sizer)
-        tur.forward(7 * sizer)
+        tur.right(15)
+        tur.forward(5 * sizer)
     tur.left(110)
-    tur.forward(80 * sizer)
-    tur.left(70)
-    tur.forward(20)
+    tur.forward(55 * sizer)
+    tur.left(65)
+    tur.penup()
+    tur.forward(30 * sizer)
+    tur.left(95)
+    tur.pendown()
 
-
-drawj(sizej = 1, Bold = True, Color = "red")
+def drawslash(sizes):
+    tur.color("red")
+    tur.forward(35 * sizes)
+    for i in range(8):
+        tur.left(15)
+        tur.forward(40 * sizes)
+    for i in range(7):
+        tur.left(15)
+        tur.forward(25 * sizes)
+    for i in range(9):
+        tur.left(10)
+        tur.forward(30 * sizes)
+    tur.left(100)
+    tur.forward(250 * sizes)
+troll = turtle.Turtle
+drawj(turtle = troll, sizej = 1, Bold = True, Color = "blue", Fast = False)
 drawr(sizer = 1)
-
+drawr(sizer = 1)
+drawslash(sizes = 1)
 turtle.mainloop()  # make screen show
